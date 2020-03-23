a8fb413273ecdb30371c805797ca4c4d8ecb60f6
hyh09 <455663056@qq.com>
2020-03-23 09:34:27 +0800

    提交Excle封装的时候采用自定义注解

---
+++ src/main/java/com/example/demo_sanm/interfacesvc/TableConFiG.java
@@ -0,0 +1,19 @@
+package com.example.demo_sanm.interfacesvc;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @单位名称：科大国创—电信资源事业部
+ * @创建人：hu.yunhui
+ * @创建时间: on 2020/3/22.
+ * @by: DELL)
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TableConFiG {
+
+    String value() default "";
+}
